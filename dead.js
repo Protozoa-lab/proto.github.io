3ed591f90b5fe79f14e9c71767f37b5ef2eaa84b
Protozoa-lab <128990933+Protozoa-lab@users.noreply.github.com>
2023-07-11 05:43:39 +0700

    dead.js

--- dead.js
+++ dead.js
@@ -1,1 +1,27 @@
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+document.onkeydown = (e) => {
+  if (
+    e.keyCode === 123 ||
+    ctrlShiftKey(e, 'I') ||
+    ctrlShiftKey(e, 'J') ||
+    ctrlShiftKey(e, 'C') ||
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  ) {
+    e.preventDefault();
+    return false;
+  }
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  var video = document.querySelector('video');
+  video.addEventListener('loadedmetadata', function () {
+    video.play();
+    video.requestFullscreen();
+  });
+});
 
